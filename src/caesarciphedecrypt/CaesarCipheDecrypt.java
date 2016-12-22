2fd405f5046b04754aafa9e420eac8561de6145a
Victoria Kachanovska <kach.vikt@gmail.com>
2016-12-22 19:35:11 +0200

    new

--- src/caesarciphedecrypt/CaesarCipheDecrypt.java
+++ src/caesarciphedecrypt/CaesarCipheDecrypt.java
@@ -5,10 +5,7 @@
  */
 package caesarciphedecrypt;
 
-import java.util.Collections;
 import java.util.HashMap;
-import static jdk.nashorn.internal.objects.NativeArray.map;
-import static jdk.nashorn.internal.objects.NativeDebug.map;
 
 /**
  *
@@ -20,11 +17,13 @@ public class CaesarCipheDecrypt {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println(decrypt("Gwpv c vbuq pvokki yfve iqqu qc bgbgbgbgbgbgbgbgbu"));// TODO code application logic here
+        System.out.println(decrypt("Kh ghwhuplqhg wr gurs klv olwljdwlrq zlwk wkh prqdvwub, dqg uholqjxlvk klv fodlpv wr wkh zrrg-fxwlqj dqg ilvkhub ulkjwv dw rqfh. Kh zdv wkh pruh uhdgb wr gr wklv ehfxdvh wkh uljkwv kdg ehfrp pxfk ohvv ydoxdeoh, dqg kh kdg lqghhg wkh ydjxhvw lghd zkhuh wkh zrrg dqg ulyhu lq txhgwlrq zhuh."));// TODO code application logic here
     }
+
+    
     public static String decrypt(String input){
-        StringBuilder replasedString = new StringBuilder(input);
         String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        StringBuilder replasedString = new StringBuilder(input);
         HashMap<Character, Integer> hashmap = new HashMap<>();
         int j = 0;
         for (int i = 0; i < input.length(); i++){
@@ -49,25 +48,25 @@ public class CaesarCipheDecrypt {
                 }
             }   
         }
-        int indexE = alphabet.indexOf('E');
-        int indexMaxChar = alphabet.indexOf(Character.toUpperCase(maxChar));
-        int key = (indexMaxChar - indexE);
-        
-        String shiftAphabet = alphabet.substring(key) + alphabet.substring(0, key);
+        int indexE = Character.getNumericValue('E');
+        int indexMaxChar = Character.getNumericValue(maxChar);
+        int key = (indexE - indexMaxChar);
         
          for (int i = 0; i < input.length(); i++){
+            char newChar = 0;
             char fromInput = input.charAt(i);
-            for (int l = 0; l < shiftAphabet.length(); l++){
-                char fromAlph = shiftAphabet.charAt(l);
-                char fromAlphLow = shiftAphabet.toLowerCase().charAt(l);
-                if (fromInput == fromAlph){
-                    char newCH = alphabet.charAt(l);
-                    replasedString.setCharAt(i, newCH);
-                } else if (fromInput == fromAlphLow){
-                    char newCH = alphabet.toLowerCase().charAt(l);
-                    replasedString.setCharAt(i, newCH);
-                }
-            }
+            int oldCharIndex = Character.getNumericValue(fromInput);
+            int newCharIndex = oldCharIndex + key;
+            if (newCharIndex > 10 && newCharIndex < 36){
+               newChar = Character.forDigit(newCharIndex, 36); 
+            } else {
+                 newChar = Character.forDigit(oldCharIndex, 36);
+             }
+            //if ((newCharIndex < 0) || (newCharIndex > 35)) {
+              //  throw new IllegalArgumentException();
+            //}
+           
+            replasedString.setCharAt(i, newChar);
         }
         return replasedString.toString(); 
     }
