9af16398d0c3d249862a05eb7ce8b2e7a9f7d75a
Viktoriia Kachanovska <kach.vikt@gmail.com>
2017-01-07 23:35:16 +0200

    OOP one key decrypt

--- src/caesarciphedecrypt/CaesarCipheDecrypt.java
+++ src/caesarciphedecrypt/CaesarCipheDecrypt.java
@@ -23,9 +23,9 @@ public class CaesarCipheDecrypt {
     }
 
     public static void testCaesarOneKeyDecrypt(){
-        FileResource fr = new FileResource();
-        String message = fr.asString();
-        String decrypted = oneKeyDecrypt(message);
+       // FileResource fr = new FileResource();
+       // String message = fr.asString();
+        String decrypted = decryptOneKey("Rpc ndj xbpvxct axut LXIWDJI iwt xcitgcti PCS rdbejitgh xc ndjg edrzti tttttttttt?");
         System.out.println(decrypted);
     }
     
@@ -35,9 +35,11 @@ public class CaesarCipheDecrypt {
         String decrypted = twoKeyDecrypt(message);
         System.out.println(decrypted);
     }
-    public static String oneKeyDecrypt(String input){
-        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        StringBuilder replasedString = new StringBuilder(input);
+    
+    private static String alphabet;
+    
+    private static HashMap parceInputString(String input){
+        alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         HashMap<Character, Integer> hashmap = new HashMap<>();
         for (int i = 0; i < input.length(); i++){
             char ch = input.toLowerCase().charAt(i);
@@ -50,26 +52,40 @@ public class CaesarCipheDecrypt {
                 }
             }
         }
+        return hashmap;
+    }
+    
+    private static char findFrequencyChar(String input){
+        HashMap hashmap = parceInputString(input);
         char maxChar = 0;
         int max = 0;
         for (int k = 0; k < 26; k++){
             char chAlphK = alphabet.toLowerCase().charAt(k);
             if (hashmap.containsKey(chAlphK)){
-                int v1 = hashmap.get(chAlphK);
+                int v1 = (int) hashmap.get(chAlphK);
                 if (max < v1){
                     max = v1;
                     maxChar = chAlphK;
                 }
             }   
         }
+        return maxChar;
+    }
+    
+    private static int getKey(String input){
+        char maxChar = findFrequencyChar(input);
         int indexE = Character.getNumericValue('E');
         int indexMaxChar = Character.getNumericValue(maxChar);
         int key = (indexE - indexMaxChar);
         if (maxChar == 'a' || maxChar == 'b' || maxChar == 'c' || maxChar == 'd'){
             key = indexE - 26 - indexMaxChar;
         }
-        System.out.println(key);
-        
+        return key;
+    }
+    
+    public static String decryptOneKey(String input){
+        StringBuilder replasedString = new StringBuilder(input);
+        int key = getKey(input);
         for (int i = 0; i < input.length(); i++){
             char newChar = 0;
             char fromInput = input.charAt(i);
@@ -86,11 +102,8 @@ public class CaesarCipheDecrypt {
                     newChar = Character.forDigit(newCharIndex, 36);
                     newChar = Character.toUpperCase(newChar);
                 }
-            } 
-            
-            
-            else {
-                newChar = Character.valueOf(fromInput);
+            } else {
+                newChar = fromInput;
              }
            
             replasedString.setCharAt(i, newChar);
@@ -99,32 +112,16 @@ public class CaesarCipheDecrypt {
     }
     
      public static String twoKeyDecrypt(String input){
-        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         StringBuilder replasedString = new StringBuilder(input);
         HashMap<Character, Integer> firstKeyHashmap = new HashMap<>();
         HashMap<Character, Integer> secondKeyHashmap = new HashMap<>();
         
         for (int i = 0; i < input.length(); i++){
             if (i%2 == 0){
-                char ch = input.toLowerCase().charAt(i);
-                if (Character.isAlphabetic(ch) ){
-                    if (firstKeyHashmap.containsKey(ch)){
-                        int newFreq = firstKeyHashmap.get(ch) + 1;
-                        firstKeyHashmap.put(ch, newFreq);
-                    } else {
-                        firstKeyHashmap.put(ch, 1);
-                    }
-                }
+                firstKeyHashmap = parceInputString(input);
             } if (i%2 != 0){
-                char ch = input.toLowerCase().charAt(i);
-                if (Character.isAlphabetic(ch) ){
-                    if (secondKeyHashmap.containsKey(ch)){
-                        int newFreq = secondKeyHashmap.get(ch) + 1;
-                        secondKeyHashmap.put(ch, newFreq);
-                    } else {
-                        secondKeyHashmap.put(ch, 1);
-                    }
-                }
+                secondKeyHashmap = parceInputString(input);
             }
         }
         
